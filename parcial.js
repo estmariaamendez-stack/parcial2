c0ba9bf8dc601d9f59122a1a1b9460dc4ce50a4f
est.mariaa.mendez@unimilitar.edu.co <est.natalia.varela@unimilitar.edu.co>
2026-04-24 13:24:38 -0500

    se crea la logica de las posiciones orbitales segun la trigonometria y sus bases teoricas.

--- parcial.js
+++ parcial.js
@@ -1,6 +1,10 @@
 /**
  * Parcial 2 - Sistema de Dispersión Geométrica Orbital
  * Computación Gráfica
+ *  Universidad Militar Nueva Granada - Ingenieria Multimedia 
+ * Asignatura: Introducción a la Computación Gráfica
+ * Estudiante: Maria Alejandra Mendez Roncancio 
+
  */
 
 /**se crea el canvas*/
@@ -19,4 +23,50 @@ const CY = HEIGHT / 2;
 function plotPixel(x, y, color = "#d6159c") {
     ctx.fillStyle = color;
     ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
+
+/**valores aleatorios de N*/
+const R = 200; // radio órbita
+const N = Math.floor(Math.random() * 7) + 4; // 4 a 10 polígonos
+
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+
+/**
+POSICIONES ORBITALES
+*@param {number} r - Radio de la órbita
+ * @param {number} n - Cantidad de polígonos
+ * return retorna la posicon de el punto
+*/
+
+function getOrbitalPositions(r, n) {
+    let positions = [];
+
+    for (let i = 0; i < n; i++) {
+        let angle = (2 * Math.PI * i) / n;
+
+        let x = centerX + r * Math.cos(angle);
+        let y = centerY + r * Math.sin(angle);
+
+        positions.push({ x, y });
+    }
+
+    return positions;
+}
+
+// -----------------------------
+// DIBUJAR ORBITE (puntos)
+// -----------------------------
+function drawOrbit() {
+    const points = getOrbitalPositions(R, N);
+
+    for (let p of points) {
+        plotPixel(ctx, p.x, p.y, "red");
+    }
+}
+
+/**se incia el funcionamiento */
+function inicializar() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawOrbit();
 }
